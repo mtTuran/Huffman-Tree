6f8e9c846ac79208e482bb23df3b6d5b1d366afb
mtTuran <turanmustafa2004@gmail.com>
2024-03-24 16:31:31 +0300

    implemented hash table to calculate the frequency of letters in a given text

--- hashing.c
+++ hashing.c
@@ -1,18 +1,76 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "hashing.h"
 
 hash_table* create_hash_table(int size){
-    return;
+    hash_table* new_map = (hash_table*) malloc(sizeof(hash_table) + sizeof(hash_item) * size);
+    new_map -> size = size;
+    new_map -> count = 0;
+
+    for (int i = 0; i < size; i++) {
+    new_map->table[i].letter = '\0';
+    new_map->table[i].frequency = 0;
+    }
+
+    return new_map;
 }
 
-int hash_function(char letter){
-    return;
+int hash_function(char letter, int collision, int size){
+    int index = ((int)letter + collision) % size;
+    return index;
 }
 
-void map_to_table(hash_table table, char letter){
-    return;
+void map_to_table(hash_table* table, char letter){
+    int collision = 0;
+    int success = 0;
+    int size = table -> size;
+    int index = hash_function(letter, collision, size);
+
+    while (!success){ 
+        if (index >= 0 && index < size && collision < size) {
+            if (table -> table[index].letter == '\0') {
+                table -> table[index].letter = letter;
+                table -> table[index].frequency += 1;
+                table -> count = table -> count + 1;
+                success = 1;
+            }
+            else if (table -> table[index].letter == letter){
+                table -> table[index].frequency += 1;
+                success = 1;
+            }
+            else {
+                collision += 1;
+                index = hash_function(letter, collision, size);
+            }
+        } else {
+            printf("An error occurred in the mapping process.\nSize of the map: %d\nCalculated index: %d\nCollision: %d", size, index, collision);
+            exit(1);
+        }
+    }
 }
 
-int search_table(char letter){
-    return;
+
+int search_table(hash_table* table, char letter){
+    int collision = 0;
+    int success = 0;
+    int size = table -> size;
+    int index = hash_function(letter, collision, size);
+
+
+    while (!success){
+        if (index >= 0 && index < size && collision < size){
+            if(table -> table[index].letter == letter || table -> table[index].letter == '\0'){
+                success = 1;
+                return table -> table[index].frequency;
+            }
+            else{
+                collision += 1;
+                index = hash_function(letter, collision, size);
+            }
+        }
+        else{
+            printf("An error occurred in the searching process.\nSize of the map: %d\nCalculated index: %d\nCollision: %d", size, index, collision);
+            exit(1);
+        }
+    }
 }
--- hashing.h
+++ hashing.h
@@ -1,4 +1,4 @@
-#ifdef HASHING_H
+#ifndef HASHING_H
 #define HASHING_H
 
 typedef struct hash_item
@@ -11,13 +11,13 @@ typedef struct hash_table
 {
     int size;
     int count;
-    hash_item table[size];
+    hash_item table[];
 }hash_table;
 
 hash_table* create_hash_table(int size);
-int hash_function(char letter);
-void map_to_table(hash_table table, char letter);
-int search_table(char letter);
+int hash_function(char letter, int collision, int size);
+void map_to_table(hash_table* table, char letter);
+int search_table(hash_table* table, char letter);
 
 
 #endif
